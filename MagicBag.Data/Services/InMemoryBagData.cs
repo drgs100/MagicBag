a7bc0d05254701190c14b20cbb7c08bc75961916
Damian Sefton <damian.sefton@nhs.net>
2020-07-29 11:56:42 +0100

    Initial commit

---
+++ MagicBag.Data/Services/InMemoryBagData.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using MagicBag.Data.Models;
+
+namespace MagicBag.Data.Services
+{
+    public class InMemoryBagData : IBagData
+    {
+        List<BagItems> bagItems;
+
+        public InMemoryBagData()
+        {
+            bagItems = new List<BagItems>()
+            {
+                new BagItems { Id = 1, Name = "Big magic sword", Type = ItemType.Weapon },
+                new BagItems { Id = 2, Name = "Magic armour", Type = ItemType.Armour },
+                new BagItems { Id = 3, Name = "Magic wand", Type = ItemType.Item },
+                new BagItems { Id = 4, Name = "Spell book", Type = ItemType.None},
+            };
+        }
+        public IEnumerable<BagItems> GetAll()
+        {
+            return bagItems.OrderBy(i => i.Name);
+        }
+    }
+}
