2ff8988251461b55b7cc1644eb79ab3dda410c87
Damian Sefton <damian.sefton@nhs.net>
2020-08-03 13:46:11 +0100

    Add file back after gitignore

---
+++ MagicBag.Data/Services/IBagData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using MagicBag.Data.Models;
+
+namespace MagicBag.Data.Services
+{
+    public interface IBagData
+    {
+        IEnumerable<BagItems> GetAll();
+    }
+}
